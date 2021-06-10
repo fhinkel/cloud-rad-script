537e70edcdbe341e339ba9a761af9a3a9e97f95c
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-06-10 18:00:26 -0400

    Update calls that send 90 PRs

--- npx-repo.sh
+++ npx-repo.sh
@@ -1,5 +1,9 @@
- npx repo exec --concurrency=1 -- "git commit -am 'chore(nodejs): delete api-extractor dependencies"
- npx repo exec --concurrency=1 -- "git push origin cloud-rad"
- npx repo exec --concurrency=1 -- "/usr/local/bin/hub pull-request -r sofisl -m 'chore(nodejs): delete api-extractor dependencies'"
- npx repo tag --title 'chore(nodejs): delete api-extractor dependencies' automerge
- npx repo exec --concurrency=1 -- "git co master "
+npx repo exec --concurrency=1 -- "git co -b fhinkel-cloud-rad-update-packagejson"
+npx repo exec --concurrency=1 -- "/Users/franzih/.nvm/versions/node/v14.5.0/bin/node ../update-devs-pack.js"
+npx repo exec --concurrency=1 -- "git commit -am 'chore(nodejs): remove api-extractor dependencies'"
+npx repo exec --concurrency=1 -- "git push origin fhinkel-cloud-rad-update-packagejson"
+export GITHUB_TOKEN=770a8ce4a1cae61d27dcf200d30413eb39c4ad7c
+npx repo exec --concurrency=1 -- "/usr/local/bin/hub pull-request -r sofisl -m 'chore(nodejs): remove api-extractor dependencies'"
+npx repo tag --title 'chore(nodejs): remove api-extractor dependencies' automerge
+npx repo exec --concurrency=1 -- "git co master"
+npx repo exec --concurrency=1 -- "git branch -D fhinkel-cloud-rad-update-packagejson"
