e28255ce8a15a7fd60084b3b440fee96ece597e4
Franziska Hinkelmann <franzih@chromium.org>
2021-08-03 15:43:51 -0400

    use full tool name

--- npx-repo-metadata.sh
+++ npx-repo-metadata.sh
@@ -3,12 +3,12 @@
 # npx repo exec --concurrency=1 -- "git co main"
 # npx repo exec --concurrency=1 -- "git fetch --prune"
 
-npx repo exec --concurrency=1 -- "git co -b fhinkel-cloud-rad-update-metadata"
-npx repo exec --concurrency=1 -- "/Users/franzih/.nvm/versions/node/v16.6.1/bin/node ../../cloud-rad-script/update-metadata.js"
-npx repo exec --concurrency=1 -- "git commit -am 'chore(nodejs): update client ref docs link in metadata'"
-npx repo exec --concurrency=1 -- "git push origin fhinkel-cloud-rad-update-metadata"
-export GITHUB_TOKEN=770a8ce4a1cae61d27dcf200d30413eb39c4ad7c
-npx repo exec --concurrency=1 -- "/usr/local/bin/hub pull-request -r sofisl -m 'chore(nodejs): update client ref docs link in metadata'"
-npx repo tag --title 'chore(nodejs): update client ref docs link in metadata' automerge
+npx @google/repo exec --concurrency=1 -- "git co -b fhinkel-cloud-rad-update-metadata"
+npx @google/repo exec --concurrency=1 -- "/Users/franzih/.nvm/versions/node/v16.6.1/bin/node ../../cloud-rad-script/update-metadata.js"
+npx @google/repo exec --concurrency=1 -- "git commit -am 'chore(nodejs): update client ref docs link in metadata'"
+npx @google/repo exec --concurrency=1 -- "git push origin fhinkel-cloud-rad-update-metadata"
+export GITHUB_TOKEN=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
+npx @google/repo exec --concurrency=1 -- "/usr/local/bin/hub pull-request -r sofisl -m 'chore(nodejs): update client ref docs link in metadata'"
+npx @google/repo tag --title 'chore(nodejs): update client ref docs link in metadata' automerge
 # npx repo exec --concurrency=1 -- "git co master"
 # npx repo exec --concurrency=1 -- "git branch -D fhinkel-cloud-rad-update-packagejson"
