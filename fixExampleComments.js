24b41d6cfa0d12d6104c18e93d58e4639f5cc95d
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-11-02 16:24:35 -0400

    Revert "delete region tag parsing"
    This reverts commit 05a4f783aba37ae3fa8f269623b683fbe142c395.

--- fixExampleComments.js
+++ fixExampleComments.js
@@ -1,6 +1,61 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const glob = require('glob');
+const { readFileSync, statSync } = require('fs');
+const { resolve } = require('path');
+
+const SAMPLES_DIRECTORY =
+  process.env.SAMPLES_DIRECTORY || resolve(process.cwd(), './samples');
+const REGION_START_REGEX = /\[START\s+([^\]]+)/;
+const REGION_END_REGEX = /\[END/;
+
+const sampleCache = new Map();
+const loadSampleCache = function () {
+  const sampleCandidates = glob.sync(`${SAMPLES_DIRECTORY}/**/*.{js,ts}`, {
+    ignore: ['node_modules'],
+  });
+  // console.log(`${SAMPLES_DIRECTORY}/**/*.{js,ts}`);
+  // console.log(sampleCandidates);
+  for (const candidate of sampleCandidates) {
+    const stat = statSync(candidate);
+    if (!stat.isFile()) continue;
+    const content = readFileSync(candidate, 'utf8');
+    if (REGION_START_REGEX.test(content)) {
+      parseSamples(content);
+    }
+  }
+  return sampleCache;
+};
+
+function parseSamples(content) {
+  let key;
+  let sample;
+  let inTag = false;
+  for (const line of content.split(/\r?\n/)) {
+    if (inTag && REGION_END_REGEX.test(line)) {
+      // console.log('found ' + key)
+      sampleCache.set(key, sample);
+      inTag = false;
+    } else if (inTag) {
+      const escapedLine = line.replace('*/', '*\\/');
+      sample += `${escapedLine}\n`;
+    } else {
+      const match = line.match(REGION_START_REGEX);
+      if (match) {
+        key = match[1];
+        sample = '';
+        inTag = true;
+      }
+    }
+  }
+}
+
+const BASE_URL = 'https://cloud.google.com/';
+function replaceRelativeLinks(description) {
+  return description.replace(/href="\//g, `href="${BASE_URL}`);
+}
+
 
 async function processLineByLine(file) {
   const fileStream = fs.createReadStream(file);
